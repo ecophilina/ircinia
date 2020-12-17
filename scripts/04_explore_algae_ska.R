92b35f03b85bbd88fce3aa184fd7271559d32101
skraftarcher <sarcher@lumcon.edu>
2020-12-17 12:37:06 -0600

    Steph's version of algae analysis

---
+++ scripts/04_explore_algae_ska.R
@@ -0,0 +1,250 @@
+library(tidyverse)
+
+source("scripts/03_reimport.R")#imports all the data sets
+if(!require(lmerTest))install.packages("lmerTest");library(lmerTest)
+if(!require(DHARMa))install.packages("DHARMa");library(DHARMa)
+if(!require(glmmTMB))install.packages("glmmTMB");library(glmmTMB)
+
+
+# ---- algaeplot ----
+a2<-algae %>%
+  pivot_wider(names_from = taxa,values_from = abundance,values_fill = list(abundance=0))%>%
+  mutate(total=rowSums(select(.,-treatment,-plot,-sampling)))%>% # whenever you are using a non-tidyverse function within a 
+  #tidyverse pipeline you have to use the "." to tell it to use the dataset you had been working with.
+  pivot_longer(4:11,names_to = "taxa",values_to = "abundance")%>%
+  mutate(prop=abundance/total,
+         season=case_when(
+           sampling==1~"summer",
+           sampling==2~"summer",
+           sampling==4~"summer",
+           sampling==3~"winter",
+           sampling==5~"winter"),
+         plot=paste(treatment,plot),
+         taxa=ifelse(taxa=="cladocephalus","udotea",taxa))
+# now I'm going to make my plot
+(c1<-ggplot(data=a2)+
+    geom_bar(aes(x=sampling,y=abundance,fill=taxa),position="stack",stat="identity")+
+    facet_wrap(~plot)+
+    geom_rect(aes(xmin=2.5,xmax=3.5,ymin=-.01,ymax=1.01),alpha=0.009,size=1,color="black")+
+    geom_rect(aes(xmin=4.5,xmax=5.5,ymin=-.01,ymax=1.01),alpha=0.009,size=1,color="black"))
+
+# looking at abundance by species over time
+
+ggplot(data=a2)+
+  geom_point(aes(x=sampling,y=abundance,color=treatment))+
+  geom_smooth(aes(x=sampling,y=abundance,color=treatment),method = "lm")+
+  facet_wrap(~taxa,scales = "free_y")
+
+a3<-a2%>%
+  filter(!taxa %in% c("brown.cyanobacteria","dictyota","green.cyanobacteria"))
+
+ggplot(data=a3)+
+  geom_point(aes(x=sampling,y=abundance,color=treatment))+
+  geom_smooth(aes(x=sampling,y=abundance,color=treatment),method = "lm")+
+  facet_wrap(~taxa,scales = "free_y")
+
+# looks like there's likely a good story here.
+
+a4<-a3 %>%
+  group_by(treatment, sampling,plot,season)%>%
+  summarize(abund=sum(abundance))
+
+ggplot(data=a4)+
+  geom_point(aes(x=sampling,y=abund,color=treatment))+
+  geom_smooth(aes(x=sampling,y=abund,color=treatment),method = "lm")
+
+# overall algal abundance increases over time
+
+# try a global model with taxa as a random effect.
+
+# get data organized
+
+a3.1<-a3%>%
+  filter(sampling==1)%>%
+  rename(start.abund=abundance)%>%
+  select(treatment, plot,taxa,start.abund)
+
+a5<-a3 %>%
+  filter(sampling!=1)%>%
+  left_join(a3.1)
+
+a5$treatment<-factor(a5$treatment)
+
+
+# Philina's custom function for checking for overdispersion
+overdisp_fun <- function(model) {
+  rdf <- df.residual(model)
+  rp <- residuals(model,type="pearson")
+  Pearson.chisq <- sum(rp^2)
+  prat <- Pearson.chisq/rdf
+  pval <- pchisq(Pearson.chisq, df=rdf, lower.tail=FALSE)
+  c(chisq=Pearson.chisq,ratio=prat,rdf=rdf,p=pval)
+}
+
+# check distributions of things
+hist(a3$total, breaks = 20) # total algal abundance
+hist((a3%>%
+       filter(taxa=="halimeda"))$abundance, breaks = 20)
+hist((a3%>%
+        filter(taxa=="laurencia"))$abundance, breaks = 20)
+hist((a3%>%
+        filter(taxa=="acetabularia"))$abundance, breaks = 20)
+hist((a3%>%
+        filter(taxa=="udotea"))$abundance, breaks = 20)
+hist((a3%>%
+        filter(taxa=="penicillus"))$abundance, breaks = 20)
+
+# try out more appropriate model
+
+a5<-a5%>%
+  mutate(year=case_when(
+    sampling==2~1,
+    sampling==3~1,
+    sampling==4~2,
+    sampling==5~2))
+
+# trying the one global model first
+alm1.r<-glmmTMB(abundance~treatment*year+treatment*season+ (1|plot)+(sampling|taxa),
+            data=a5%>%
+              mutate(treatment=relevel(treatment, ref = "real")),
+            offset=log(start.abund+1),
+            REML=F,
+            family=nbinom2)
+
+alm1.f<-glmmTMB(abundance~treatment*year+treatment*season+ (1|plot)+(sampling|taxa),
+                data=a5%>%
+                  mutate(treatment=relevel(treatment, ref = "fake")),
+                offset=log(start.abund+1),
+                REML=F,
+                family=nbinom2)
+
+
+alm1.b<-glmmTMB(abundance~treatment*year+treatment*season+ (1|plot)+(sampling|taxa),
+                data=a5%>%
+                  mutate(treatment=relevel(treatment, ref = "blank")),
+                offset=log(start.abund+1),
+                REML=F,
+                family=nbinom2)
+
+
+
+# look at residuals
+alm1_simres <- simulateResiduals(alm1)
+testDispersion(alm1_simres)
+plot(alm1_simres)
+
+# residuals look meh
+(alm1r.sum<-summary(alm1.r))
+(alm1f.sum<-summary(alm1.f))
+(alm1b.sum<-summary(alm1.b))
+
+
+
+
+# BUT we now have a significant increase over time in real treatment that is 
+# significantly different than the other treatments.
+
+# Now talk through whether or not the figure I made is appropriate.
+
+
+a5<-a5%>%
+  mutate(samp2=case_when(
+    sampling==2~1,
+    sampling==3~5,
+    sampling==4~12,
+    sampling==5~17))
+
+# potential figure
+a6<-a3 %>%
+  select(treatment, plot,sampling,total,season)%>%
+  distinct()
+
+a6.1<-a6%>%
+  filter(sampling==1)%>%
+  rename(start.total=total)%>%
+  select(-sampling,-season)
+
+a6<-a6 %>%
+  filter(sampling!=1)%>%
+  left_join(a6.1)%>%
+  mutate(samp2=case_when(
+    sampling==2~1,
+    sampling==3~5,
+    sampling==4~12,
+    sampling==5~17),
+    year=case_when(
+      sampling==2~1,
+      sampling==3~1,
+      sampling==4~2,
+      sampling==5~2))
+
+a6$treatment<-factor(a6$treatment)
+a6$season<-factor(a6$season)
+
+
+a6<-a6%>%
+  mutate(delta.tot=total-start.total)
+a7<-a6%>%
+  group_by(treatment,samp2)%>%
+  summarize(m.tot=mean(delta.tot),sd.tot=sd(delta.tot))
+ylab<-expression(paste(Delta," macroalgae abundance"))
+
+(afig <- ggplot() +
+    geom_hline(aes(yintercept = 0)) +
+    geom_point(
+      data = a6,
+      aes(x = samp2, y = delta.tot, color = treatment),
+      position = position_dodge(1.8)) +
+    geom_point(data = a7,
+      aes(x = samp2, y = m.tot, color = treatment),
+      size = 8,
+      position = position_dodge(+1.8),
+      alpha = .5) +
+    geom_errorbar(
+      data = a7,
+      aes(x = samp2,ymin = m.tot - sd.tot,ymax = m.tot + sd.tot,color = treatment),
+      width = 1,
+      position = position_dodge(1.8)) +
+    theme_bw() +
+    xlab("Months into experiment") +
+    ylab(ylab) +
+    scale_color_brewer(type = "qual",
+      palette = "Set2",
+      name = "",
+      labels = c("Control", "Structure control", "Sponge")) +
+    theme_bw() +
+    theme(panel.grid = element_blank(),
+      panel.spacing = unit(0, "lines"),
+      strip.background = element_blank(),
+      legend.position = "top",
+      legend.text = element_text(size = 14),
+      axis.title = element_text(size = 14),
+      axis.text = element_text(size = 12),
+      strip.text = element_text(size = 14)))
+ggsave("figures/algal_abundance.jpg")
+
+# making a more appropriate figure
+
+a5<-a5%>%
+  mutate(delta.abund=abundance-start.abund)
+
+a5$treatment<-factor(a5$treatment,levels=c("blank","fake","real"),labels=c("Control","Structure Control","Sponge"))
+my.labels <- paste0(c("S","W","S","W"), "\n", c("Year 1","Year 1","Year 2", "Year 2"))
+
+ggplot(data=a5)+
+  geom_point(aes(x=sampling,y=abundance,color=taxa),position=position_dodge(0.5))+
+  scale_x_continuous(name="",breaks=c(2,3,4,5),label = my.labels)+
+  facet_grid(~treatment)+
+  theme_bw()+
+  theme(panel.grid = element_blank(),
+        legend.title.align = 0.5,
+        strip.background = element_blank(),
+        strip.text = element_text(size=14))+
+  geom_hline(aes(yintercept=0))+
+  ylab("Macroalgal Abundance")+
+  scale_color_brewer(palette = "Set2",name="Taxa",labels=c("Acetabularia",
+                                                           "Halimeda",
+                                                           "Laurencia",
+                                                           "Penicillus",
+                                                           "Udotea"))
+
