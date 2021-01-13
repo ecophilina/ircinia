5fbf2e769631319726398b4b39b718d444e045f3
finella123 <finella.campanino@gmail.com>
2021-01-13 11:25:27 -0600

    Added all packages from our scripts and noted RTools download

--- scripts/01_installpackage.R
+++ scripts/01_installpackage.R
@@ -3,6 +3,8 @@
 
 ## @knitr loadpackages
 # load packages ####
+# these should be all packages needed to run all scripts in the Ircinia Project. 
+#**note** If you are using a new computer, you may need to go to https://cran.r-project.org/bin/windows/Rtools/ website and download RTools, if there are errors when running the scripts. 
 
 if(!require(here))install.packages('here');library(here)
 if(!require(tidyverse))install.packages('tidyverse');library(tidyverse)
@@ -12,3 +14,11 @@ if(!require(vegan))install.packages('vegan');library(vegan)
 if(!require(usethis))install.packages('usethis')
 #usethis::use_git_config(user.name = "username", user.email = "email")
 if(!require(bookdown))install.packages('bookdown')
+
+
+if(!require(lmerTest))install.packages("lmerTest");library(lmerTest)
+if(!require(DHARMa))install.packages("DHARMa");library(DHARMa)
+if(!require(glmmTMB))install.packages("glmmTMB");library(glmmTMB)
+if(!require(ggeffects))install.packages("ggeffects");library(ggeffects)
+if(!require(car))install.packages("car");library(car)
+if(!require(ggpubr))install.packages("ggpubr");library(ggpubr)
