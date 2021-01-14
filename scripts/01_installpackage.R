679af28eacf75e06bc6a21eb11170810bf6b7246
Finella Campanino <fcampanino@LUMCON.edu>
2021-01-14 16:04:56 -0600

    Added more about RTools and 2 more packages

--- scripts/01_installpackage.R
+++ scripts/01_installpackage.R
@@ -4,7 +4,10 @@
 ## @knitr loadpackages
 # load packages ####
 # these should be all packages needed to run all scripts in the Ircinia Project. 
-#**note** If you are using a new computer, you may need to go to https://cran.r-project.org/bin/windows/Rtools/ website and download RTools, if there are errors when running the scripts. 
+#**note** If you are using a new computer, you may need to go to https://cran.r-project.org/bin/windows/Rtools/ website and download RTools, if there are errors when running the scripts.
+#If Rstudio still says you do not have RTools use the 2 lines of code below
+#writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+#Sys.which("make")
 
 if(!require(here))install.packages('here');library(here)
 if(!require(tidyverse))install.packages('tidyverse');library(tidyverse)
@@ -22,3 +25,5 @@ if(!require(glmmTMB))install.packages("glmmTMB");library(glmmTMB)
 if(!require(ggeffects))install.packages("ggeffects");library(ggeffects)
 if(!require(car))install.packages("car");library(car)
 if(!require(ggpubr))install.packages("ggpubr");library(ggpubr)
+if(!require(egg))install.packages("egg")
+if(!require(cowplot))install.packages("cowplot");library(cowplot)
