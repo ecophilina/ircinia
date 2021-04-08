91d35fe58e042a2efb2a638e04bbd48da78d2a41
ecophilina <paenglis@lakeheadu.ca>
2021-04-08 14:59:17 -0700

    move packages unique to community analysis to new script

--- scripts/01_installpackage.R
+++ scripts/01_installpackage.R
@@ -12,7 +12,6 @@
 if(!require(here))install.packages('here');library(here)
 if(!require(tidyverse))install.packages('tidyverse');library(tidyverse)
 if(!require(readxl))install.packages('readxl');library(readxl)
-if(!require(vegan))install.packages('vegan');library(vegan)
 
 if(!require(usethis))install.packages('usethis')
 #usethis::use_git_config(user.name = "username", user.email = "email")
---
+++ scripts_comm/01_comm_packages.R
@@ -0,0 +1,33 @@
+## this script is only to load packages used in community analysis ##
+## @knitr loadpackages
+# load packages ####
+#**note** If you are using a new computer, you may need to go to https://cran.r-project.org/bin/windows/Rtools/ website and download RTools, if there are errors when running the scripts.
+#If Rstudio still says you do not have RTools use the 2 lines of code below
+#writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+#Sys.which("make")
+
+if(!require(here))install.packages('here')
+if(!require(tidyverse))install.packages('tidyverse')
+if(!require(readxl))install.packages('readxl')
+if(!require(vegan))install.packages('vegan')
+
+if(!require(usethis))install.packages('usethis')
+#usethis::use_git_config(user.name = "username", user.email = "email")
+if(!require(bookdown))install.packages('bookdown')
+
+
+if(!require(lmerTest))install.packages("lmerTest")
+if(!require(DHARMa))install.packages("DHARMa")
+if(!require(vegan))install.packages('performance')
+
+if(!require(glmmTMB))install.packages("glmmTMB")
+if(!require(ggeffects))install.packages("ggeffects")
+if(!require(car))install.packages("car")
+if(!require(ggpubr))install.packages("ggpubr")
+if(!require(egg))install.packages("egg")
+if(!require(cowplot))install.packages("cowplot")
+
+# install.packages("devtools")
+if(!require(ggnewscale))devtools::install_github("eliocamp/ggnewscale")
+
+install.packages("see", dependencies = TRUE)
