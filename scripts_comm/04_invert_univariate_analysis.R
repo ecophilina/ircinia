8d639c0458e2bcbb2398a517de7361ec1ab69314
ecophilina <paenglis@lakeheadu.ca>
2021-04-22 13:12:31 -0700

    test effects of prod and structure on richness

--- scripts_comm/04_invert_univariate_analysis.R
+++ scripts_comm/04_invert_univariate_analysis.R
@@ -7,6 +7,7 @@ library(glmmTMB)
 library(DHARMa)
 library(pacman)
 pacman::p_load(s20x, lme4, AICcmodavg, MASS)
+
 source("scripts_comm/02_community_data_org.R") 
 
 # visualize data
@@ -211,3 +212,105 @@ for(i in 1:length(cand.mod.names)) {
 # Function aictab does the AICc-based model comparison
 print(aictab(cand.set = cand.mods, 
              modnames = cand.mod.names))
+
+
+# What if we start with breaking downt the effect of treatments on richness, eveness and diversity?
+
+(i.sponge.model<-performance::r2(idiv.glmm))
+(i.sponge.model<-performance::r2(ispr.glmm))
+(i.sponge.model<-performance::r2(ij.glmm))
+
+# since richness is much more related to treament than diversity, maybe the test of the imporance of prod and structure should be asked for richness instead (or in addition to for diversity)? 
+
+# seagrass productivity model for richness
+ispr.prod.glmm<-glmmTMB::glmmTMB(change.spr~sg.prod.c*as.factor(sampling) + 
+    (1|plot),
+  #family= poisson,
+  data = inv.uni2%>%
+    filter(season=="summer")%>%
+    mutate(treatment=relevel(treatment, ref = "real")))
+
+summary(ispr.prod.glmm)
+(ispr.prod.model<-performance::r2(ispr.prod.glmm))
+# ispr.prod.glmm_simres <- simulateResiduals(ispr.prod.glmm)
+# testDispersion(ispr.prod.glmm_simres)
+# plot(ispr.prod.glmm_simres)
+
+
+# combine treatment and seagrass productivity model for richness
+ispr.treat.prod.glmm<-glmmTMB::glmmTMB(change.spr~
+    treatment*as.factor(sampling) + 
+    sg.prod.c*as.factor(sampling) + 
+    (1|plot),
+  #family= poisson,
+  data = inv.uni2%>%
+    filter(season=="summer")%>%
+    mutate(treatment=relevel(treatment, ref = "real")))
+
+summary(ispr.treat.prod.glmm)
+(ispr.prod.model<-performance::r2(ispr.treat.prod.glmm))
+# ispr.treat.prod.glmm_simres <- simulateResiduals(ispr.treat.prod.glmm)
+# testDispersion(ispr.treat.prod.glmm_simres)
+# plot(ispr.treat.prod.glmm_simres)
+
+# seagrass structure model for richness
+ispr.sg.struct.glmm<-glmmTMB::glmmTMB(change.spr~sg.sd.c*as.factor(sampling) + 
+    (1|plot),
+  #family= poisson,
+  data = inv.uni2%>%
+    filter(season=="summer")%>%
+    mutate(treatment=relevel(treatment, ref = "real")))
+
+summary(ispr.sg.struct.glmm)
+(ispr.sg.struct.model<-performance::r2(ispr.sg.struct.glmm))
+# ispr.sg.struct.glmm_simres <- simulateResiduals(ispr.sg.struct.glmm)
+# testDispersion(ispr.sg.struct.glmm_simres)
+# plot(ispr.sg.struct.glmm_simres)
+
+# combine treatment and struct for richness
+ispr.sg.treat.struct.glmm<-glmmTMB::glmmTMB(change.spr~
+    treatment*as.factor(sampling) + 
+    sg.sd.c*as.factor(sampling) + 
+    (1|plot),
+  #family= poisson,
+  data = inv.uni2%>%
+    filter(season=="summer")%>%
+    mutate(treatment=relevel(treatment, ref = "real")))
+
+summary(ispr.sg.treat.struct.glmm)
+(ispr.treat.struct.model<-performance::r2(ispr.sg.treat.struct.glmm))
+# idiv.sg.treat.struct.glmm_simres <- simulateResiduals(ispr.sg.treat.struct.glmm)
+# testDispersion(ispr.sg.treat.struct.glmm)
+# plot(idiv.sg.treat.struct.glmm_simres)
+
+(i.sponge.model<-performance::r2(ispr.glmm))
+(ispr.prod.model<-performance::r2(ispr.prod.glmm))
+(ispr.treat.prod.model<-performance::r2(ispr.treat.prod.glmm))
+(ispr.sg.struct.model<-performance::r2(ispr.sg.struct.glmm))
+(ispr.treat.struct.model<-performance::r2(ispr.sg.treat.struct.glmm))
+
+
+# AIC(ispr.glmm, ispr.prod.glmm, ispr.treat.prod.glmm, ispr.sg.struct.glmm, ispr.sg.treat.struct.glmm)
+
+MuMIn::AICc(ispr.glmm) 
+MuMIn::AICc(ispr.prod.glmm)
+MuMIn::AICc(ispr.treat.prod.glmm)
+MuMIn::AICc(ispr.sg.struct.glmm)
+MuMIn::AICc(ispr.sg.treat.struct.glmm)
+
+
+# Model selection
+cand.mod.names <- c("ispr.glmm", "ispr.prod.glmm", "ispr.treat.prod.glmm", "ispr.sg.struct.glmm", "ispr.sg.treat.struct.glmm")
+cand.mods <- list( ) 
+
+# This function fills the list by model names
+for(i in 1:length(cand.mod.names)) {
+  cand.mods[[i]] <- get(cand.mod.names[i]) }
+
+# Function aictab does the AICc-based model comparison
+print(aictab(cand.set = cand.mods, 
+  modnames = cand.mod.names))
+
+
+
+
