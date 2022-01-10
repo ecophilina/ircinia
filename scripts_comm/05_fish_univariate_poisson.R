0e87a091385218d835eeb83e837d2372ae9218de
Finella Campanino <fcampanino@LUMCON.edu>
2022-01-10 11:39:22 -0600

    total taxa obs added

--- scripts_comm/05_fish_univariate_poisson.R
+++ scripts_comm/05_fish_univariate_poisson.R
@@ -9,6 +9,13 @@ library(tidyverse)
 
 source("scripts_comm/02_community_data_org.R")
 
+#view total number of fish taxa observed in plots (15)
+fish %>% 
+  filter(abundance > 0) %>% 
+  select(taxa) %>% 
+  distinct() %>% 
+  View()
+
 # visualize data
 ggplot(fish.uni) +
   geom_jitter(aes(
